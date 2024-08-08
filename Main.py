e563bc8ca52ca6b8aa9d1e78c41b1e35734793c9
Arjun shrestha <shresthaarjunofficial11@gmail.com>
2024-08-08 18:44:16 -0500

    labeled data,splited test and train dataSets. Modified few issues

--- Main.py
+++ Main.py
@@ -6,9 +6,12 @@ import emoji
 import nltk
 import contractions
 import re
+
+from sklearn.model_selection import train_test_split
 from textblob import TextBlob
 from transformers import RobertaTokenizer
 from transformers import TFRobertaModel
+from sklearn.preprocessing import LabelEncoder
 from keras.layers import Input, LSTM, Dense, Dropout
 #  Bypass SSL verification
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -56,27 +59,38 @@ def cleanText(text):
 
 # Applying cleaning Function
 data['clean_data'] = data['clean_text'].apply(cleanText)
+def get_Sentiments(text):
+    analysis = TextBlob(text)
+    # Let's Determine the  sentiment Polarity
+    if analysis.sentiment.polarity > 0:
+        return 'Positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'Neutral'
+    else:
+        return 'Negative'
+# Apply Sentiments Labeling
+data['sentiment'] = data['clean_data'].apply(get_Sentiments)
+print(data[['clean_data','sentiment']].head())
 
-# Loading the RoBERTa tokenizer
+# Let's Encode the sentiments.
+label_encoder = LabelEncoder()
+data['label'] = label_encoder.fit_transform(data['sentiment'])
+# Now, Let's split the data set , Training and Testing dataSet
+train_data , test_data = train_test_split(data,test_size=0.2, random_state=42)
+# Let's Load the RoBERTa tokenizer
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-
 # Let's create a function to tokenize a data
 def tokenize_text(text):
     return tokenizer(text, truncation=True, padding='max_length', max_length=512)
-
 # Let's apply the tokenization function
-data['tokenized'] = data['clean_data'].apply( lambda  x: tokenize_text(x))
-# Let's extract it to view the Id's and attention mask
-def Extratcions(tokenized):
-    return tokenized['input_ids']
+train_data['tokenized'] = train_data['clean_data'.apply(lambda x: tokenize_text(x))]
+test_data['tokenized'] = test_data['clean_data'].apply(lambda x : tokenize_text(x))
+# Let's Extract input ids and Attention mask from the Tokenized Data
+train_data['input_ids'] = train_data['tokenized'].apply(lambda x: x['input_ids'])
+train_data['attention_mask'] = train_data['tokenized'].apply(lambda x: x ['attention_mask'])
 
-data['input_ids'] = data['tokenized'] .apply(lambda x: x['input_ids'])
-data['attention_mask'] = data['tokenized'].apply(lambda x: x['attention_mask'])
-
-# Print Tokenization
-# print(" Tokenize text\n ", data['clean_data'].head() ,'\n', data['tokenized'].head())
-print("Extractions Id's" , data[['input_ids', 'attention_mask']].head())
-# Now it's time to Fine Tune withe LSTM RNN model to further Process.
+test_data['input_ids'] = test_data['tokenized'].apply(lambda x: x['input_ids'])
+test_data['attention_mask'] = test_data['tokenized'].apply(lambda x: x ['attention_mask'])
 # Loading the pre-train model  Roberta Model , using to understand the meaning of the sentences(Vectorization)
 roberta_model = TFRobertaModel.from_pretrained('roberta-base')
 # Defining The input Layers
@@ -84,3 +98,4 @@ input_ids_layer =  Input(shape=(512), dtype= 'int32', name = 'input_ids')
 attention_mask_layer = Input(shape=(512), dtype = 'int32', name = 'attention_mask')
 # Let's Get the Roberta Embeddings now
 roberta_embeddings = roberta_model(input_ids_layer,attention_mask_layer)[0]
+# Lets Add  LSTM Layer for further Processing now
